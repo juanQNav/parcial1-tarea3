3baf8d83f9d8391eec0c9535cd7ab04b253dc2ef
juanQNav <juan.jlqn@gmail.com>
2023-01-29 20:36:11 -0600

    Ejercicio tarea 3 parcial 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+      //Las clases se nombran con UpperCammelCase
+      //Las variables y los métodos de nombran con loweCammelCase
+        int operationIndex=0;
+
+        do {
+            String operation=args[operationIndex];
+            int parameter1 = Integer.parseInt(args[operationIndex + 1]);
+            int parameter2 = Integer.parseInt(args[operationIndex + 2]);
+
+            switch (operation) {
+                case "SUMA" ->
+                        System.out.println(parameter1 + " + " + parameter2 + " = " + (parameter1 + parameter2));
+                case "MULTIPLICACION" ->
+                        System.out.println(parameter1 + " * " + parameter2 + " = " + (parameter1 * parameter2));
+                case "DIVISION" ->
+                        System.out.println(parameter1 + " / " + parameter2 + " = " + (parameter1 / parameter2));
+                case "RESTA" ->
+                        System.out.println(parameter1 + " - " + parameter2 + " = " + (parameter1 - parameter2));
+            }
+            operationIndex+=3;
+        }while(operationIndex<args.length);
+    }
+}
